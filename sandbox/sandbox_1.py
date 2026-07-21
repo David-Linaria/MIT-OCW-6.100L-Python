2ecc6b103f007e7e4b683bfda145031aa1bb879c
davidlinaria <davidlinaria@gmail.com>
2026-07-21 17:17:39 +0800

    lecture 7 decomposition, abstraction, and functions.

--- sandbox/sandbox_1.py
+++ sandbox/sandbox_1.py
@@ -7,16 +7,25 @@
 # print(f'\nAfter the loop, n is {n}')
 # # It shows that n is exactly the n in the main block. However, n will starts at 0 despite its initial value.
 
-# How return functions in python
-def main():
-    x = int(input('Please put in a number and we will check if it is a perfect square: '))
-    guess = 0
-    while guess ** 2 < x:
-        guess += 1
-    if guess ** 2 == x:
-        print(f'{x} is the square of {guess}')
-        return
-    print(f'{x} is not a perfect square.')
+# # How return functions in python
+# def main():
+#     x = int(input('Please put in a number and we will check if it is a perfect square: '))
+#     guess = 0
+#     while guess ** 2 < x:
+#         guess += 1
+#     if guess ** 2 == x:
+#         print(f'{x} is the square of {guess}')
+#         return
+#     print(f'{x} is not a perfect square.')
+#
+# main()
+# # oh now i am clear about how main function works. great.
 
-main()
-# oh now i am clear about how main function works. great.
+# """ What if the function returns a string? can it be used in print()?"""
+# def return_str():
+#     x = 3
+#     return f'{x} it can be printed'
+#
+# return_str()
+# print(return_str())
+# # from this example we can see that returning a sentence is valid in python, and it could be called in print().
