9882c3b35b63c9e5b92f65867189ab2a76496d13
davidlinaria <davidlinaria@gmail.com>
2026-08-06 12:10:48 +0800

    lec 14 Dictionary

---
+++ finger_exercises/finger_exercise_lec14.py
@@ -0,0 +1,20 @@
+def keys_with_value(aDict, target):
+    """
+    aDict: a dictionary
+    target: an integer or string
+    Assume that keys and values in aDict are integers or strings.
+    Returns a sorted list of the keys in aDict with the value target.
+    If aDict does not contain the value target, returns an empty list.
+    """
+    # Your code here
+    target_key_list = []
+    for k,v in list(aDict.items()):
+        if v == target:
+            target_key_list.append(k)
+
+    return target_key_list
+
+# Examples:
+aDict = {1:2, 2:4, 5:2}
+target = 2
+print(keys_with_value(aDict, target)) # prints the list [1,5]
